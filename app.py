1e449351d076b937dea921b8c38baad1a6556c96
sai saketh <kamatamsaketh@gmail.com>
2024-09-23 12:58:21 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from wtforms.validators import InputRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data  
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))) 
+        return "File has been uploaded sucessfully."
+    return render_template('index.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
